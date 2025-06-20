a2fcb7ab346e12ab87f475f740329f7492c04063
sanjeevkansyakar <sanjeevkansyakar789@gmail.com>
2025-06-20 17:00:13 +0530

    add @vercel/analytics package and integrate into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
@@ -14,33 +15,34 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Sanjeev Kansyakar | Portfolio",
-    description: "Sanjeev is Next.js full stack developer.",
+  title: "Sanjeev Kansyakar | Portfolio",
+  description: "Sanjeev is Next.js full stack developer.",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" className="!scroll-smooth">
-            <body
-                className={`${inter.className} bg-mainBG relative dark:bg-darkBlack`}
-            >
-                <ThemeContextProvider>
-                    <ActiveSectionContextProvider>
-                        <div className="flex flex-col flex-1 relative">
-                            <Header />
-                            {children}
-                            <Footer />
-                        </div>
+  return (
+    <html lang="en" className="!scroll-smooth">
+      <body
+        className={`${inter.className} bg-mainBG relative dark:bg-darkBlack`}
+      >
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <div className="flex flex-col flex-1 relative">
+              <Header />
+              {children}
+              <Analytics />
+              <Footer />
+            </div>
 
-                        <Toaster position="top-right" />
-                        <ThemeSwitch />
-                    </ActiveSectionContextProvider>
-                </ThemeContextProvider>
-            </body>
-        </html>
-    );
+            <Toaster position="top-right" />
+            <ThemeSwitch />
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
+      </body>
+    </html>
+  );
 }
